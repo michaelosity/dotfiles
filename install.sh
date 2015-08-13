c441c06a44be4caee0145a0be4e7482b5234909b
Michael Wells <michael.wells@3dsystems.com>
2015-08-13 15:31:59 -0700

    Adding install.sh and vim colorschemes

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo
+echo "DOTFILES"
+
+for f in "bashrc" "bash_prompt" "vimrc" "profile"; do
+    DOTFILE="${HOME}/.${f}"
+    if [[ -e "${DOTFILE}" && -h "${DOTFILE}" ]]; then
+        echo "  ~/.${f} already exists"
+    else
+        echo "  Creating ~/.${f}"
+        ln -s "${PWD}/${f}" "${HOME}/.${f}"
+    fi
+done
+
+echo
+echo "VIM COLORS"
+if [ -d "${HOME}/.vim" ]; then 
+    echo "  ~/.vim already exists"
+else
+    ln -s "${PWD}/vim" "${HOME}/.vim"
+fi
+
+echo
+echo "DONE"
+echo
